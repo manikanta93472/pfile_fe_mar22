e8467b3287c9fb4920662e52a866646bebfc0914
manikanta93-git <manikanta29159@gmail.com>
2025-03-19 09:38:56 +0530

    add

---
+++ js_programs/p2.js
@@ -0,0 +1,15 @@
+
+console.log(lessThan30)
+
+let employees = [
+    {name:'dravid',sal:20000},
+    {name:'rohit',sal:30000},
+    {name:'dhoni',sal:25000}
+];
+let names = employees.map((emp) => {
+    return emp.name
+})
+console.log(names)
+
+let filteredEmps = employees.filter((emp) => {
+    return emp.sal <= 25000
